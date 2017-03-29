5a2817900e1ed0d421770b4827aafe50cccb0384
Yuliya <bothoi@mail.ru>
2017-03-29 16:19:02 +0800

    new class

---
+++ src/courserob/NewClass.java
@@ -0,0 +1,18 @@
+
+package courserob;
+
+/**
+ *
+ * @author Евгений
+ */
+public class NewClass {
+    
+   public NewClass(){
+   }
+
+   public NewClass(String name){
+      // Так выглядит конструктор в Java и у него один параметр, name.
+   
+   }
+
+}
